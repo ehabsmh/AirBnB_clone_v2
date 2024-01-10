b4ede5d52d6c32fb1ddf95734c8b94b4a55ef9a7
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-11 00:29:00 +0200

    1. Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive"""
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static\
+     folder of your AirBnB Clone repo"""
+    yr = datetime.utcnow().year
+    mth = datetime.utcnow().month
+    day = datetime.utcnow().day
+    hr = datetime.utcnow().hour
+    mins = datetime.utcnow().min
+    secs = datetime.utcnow().second
+    
+    directory = "versions"
+    
+    archive = f"web_static_{yr}{mth}{day}{hr}{mins}{secs}.tgz"
+    
+    if not os.path.isdir(directory):
+        if local(f"mkdir {directory}").failed:
+            return None
+    
+    if local(f"tar -czvf {archive} web_static/").failed:
+        return None
+    
+    return os.path.abspath(archive)
+    
+    
+    
