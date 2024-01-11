b8f7d3527df3b8ad7bf146dcdf9f7f8dd59270c2
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-11 20:16:54 +0200

    .

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from
-the contents of the web_static folder of your AirBnB Clone
-repo, using the function do_pack.
-"""
-from datetime import datetime
+"""Generates a .tgz archive"""
+
 from fabric.api import local
+from datetime import datetime
 import os
 
 
 def do_pack():
-    """
-    Generate .tgs compress.
-    """
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except BaseException:
+    """generates a .tgz archive from the contents of the web_static\
+     folder of your AirBnB Clone repo"""
+    yr = datetime.utcnow().year
+    mth = datetime.utcnow().month
+    day = datetime.utcnow().day
+    hr = datetime.utcnow().hour
+    mins = datetime.utcnow().minute
+    secs = datetime.utcnow().second
+
+    directory = "versions"
+
+    archive = f"{directory}/web_static_{yr}{mth}{day}{hr}{mins}{secs}.tgz"
+
+    if not os.path.isdir(directory):
+        if local(f"mkdir -p {directory}").failed:
+            return None
+
+    if local(f"tar -czvf {archive} web_static").failed:
         return None
+
+    return archive
