7d37c6e53ab6c0a6d8b7ca3cd1e0e11f7adccda3
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-11 19:41:49 +0200

    refactored

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,58 +9,46 @@ env.hosts = ['54.242.168.59', '52.206.252.73']
 
 def do_deploy(archive_path):
     """Distributes an archive to web servers
-    Args: 
+    Args:
         archive_path: The path to the archive file
-    
+
     Returns:
         True, Otherwise - False.
     """
 
     # Check if file at the path archive_path doesn’t exist
-    if not os.path.isfile(archive_path):
+    if not os.path.exists(archive_path):
         return False
 
-    archive_file = archive_path.split("/")[-1]
-    archive_name = archive_file.split(".")[0]
-    releases_path = "/data/web_static/releases"
+    try:
+        archive_file = archive_path.split("/")[-1]
+        archive_name = archive_file.split(".")[0]
+        releases_path = "/data/web_static/releases"
 
-    # Upload the archive to the /tmp/ directory of the web server
-    if put(archive_path, f"/tmp/{archive_file}").failed:
-        return False
-    
-    if run(f"rm -rf {releases_path}/{archive_name}/").failed:
-        return False
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
 
-    # Create the release directory for ex: web_static_20170315003959
-    if run(f"mkdir -p {releases_path}/{archive_name}/").failed:
-        return False
+        # Create the release directory for ex: web_static_20170315003959
+        run(f"mkdir -p {releases_path}/{archive_name}/")
 
-    # Uncompress the archive
-    if run(f"tar -xzf /tmp/{archive_file} -C {releases_path}/{archive_name}/"
-           ).failed:
-        return False
+        # Uncompress the archive
+        run(f"tar -xzf /tmp/{archive_file} -C {releases_path}/{archive_name}/")
 
-    # Delete the archive file
-    if run(f"rm /tmp/{archive_file}").failed:
-        return False
+        # Delete the archive file
+        run(f"rm /tmp/{archive_file}").failed
 
-    # Move all the extracted content to our release directory
-    if run(f"mv {releases_path}/{archive_name}/web_static/* "
-           f"{releases_path}/{archive_name}/").failed:
-        return False
+        # Move all the extracted content to our release directory
 
-    # Remove the web_static directory
-    if run(f"rm -rf {releases_path}/{archive_name}/web_static"
-           ).failed:
-        return False
+        # Remove the web_static directory
+        run(f"rm -rf {releases_path}/{archive_name}/web_static")
 
-    # Delete symbolic link
-    if run("rm -rf /data/web_static/current").failed:
-        return False
+        # Delete symbolic link
+        run("rm -rf /data/web_static/current")
 
-    # Create a new the symbolic link
-    if run(f"ln -s {releases_path}/{archive_name} /data/web_static/current"
-           ).failed:
-        return False
+        # Create a new the symbolic link
+        run(f"ln -s {releases_path}/{archive_name} /data/web_static/current")
 
-    return True
+        print("New version deployed!")
+        return True
+    except BaseException:
+        return False
