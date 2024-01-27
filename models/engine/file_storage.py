1faa71426637da8f74c7976453f83b8e4eff3cb3
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-27 19:18:53 +0200

    7. Improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,9 +19,13 @@ class FileStorage:
         else:
             return FileStorage.__objects
 
+    # _________________________________________________________________________________
+
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    # _________________________________________________________________________________
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -31,6 +35,8 @@ class FileStorage:
 
         with open(FileStorage.__file_path, 'w', encoding='utf8') as f:
             json.dump(temp, f)
+
+    # _________________________________________________________________________________
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -56,9 +62,17 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
+    # _________________________________________________________________________________
+
     def delete(self, obj=None):
         """Deletes obj from __objects if it is inside"""
         if obj:
             key = f"{obj.__class__.__name__}.{obj.id}"
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+
+    # _________________________________________________________________________________
+
+    def close(self):
+        """deserializing the JSON file to objects"""
+        self.reload()
